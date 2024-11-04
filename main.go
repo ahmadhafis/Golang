617fa185bfec80be0ea892fd9cc182b5f5419dd9
ahmadhafis <ahmadhafis296@gmail.com>
2024-11-04 14:06:18 +0700

    update

--- main.go
+++ main.go
@@ -10,6 +10,11 @@ func main() {
 
 	fmt.Println("Welcome")
 	fmt.Println("Nilai MagicSum: ", utils.MagicSum(n))
-	
+	fmt.Println("Nilai MagicPow: ", utils.MagicPow(n))
+	fmt.Println("Nilai MagicOdd: ", utils.MagicOdd(n))
+	fmt.Println("Nilai MagicGrade: ", utils.MagicGrade(n))
+	fmt.Println("Nilai MagicName: ", utils.MagicName(n))
+	fmt.Println("Nilai MagicTria: ", utils.MagicTria(n))
+
 
 }
